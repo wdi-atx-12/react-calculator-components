e1145fa3650fc0973dc949da267ebf81f12c1b76
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-04-10 16:27:03 -0500

    pushing up the original files

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>React Calculator!</h1>
+        <div className="box">
+          <div className="display">
+            <input type="text" readonly size="18" id="d"/>
+          </div>
+          <div className="keys">
+            <p>
+              <input type="button" className="button gray" value="mrc" />
+              <input type="button" className="button gray" value="m-" />
+              <input type="button" className="button gray" value="m+" />
+              <input type="button" className="button pink" value="/" />
+            </p>
+
+            <p>
+            <input type="button" className="button black" value="7" />
+              <input type="button" className="button black" value="8" />
+                <input type="button" className="button black" value="9" />
+                <input type="button" className="button pink" value="*" />
+              </p>
+              <p>
+                <input type="button" className="button black" value="4" />
+                <input type="button" className="button black" value="5" />
+                  <input type="button" className="button black" value="6" />
+                  <input type="button" className="button pink" value="-" />
+                  </p>
+                <p>
+                  <input type="button" className="button black" value="1" />
+                <input type="button" className="button black" value="2" />
+                  <input type="button" className="button black" value="3" />
+                  <input type="button" className="button pink" value="+" />
+
+                  </p>
+                <p>
+                  <input type="button" className="button black" value="0" />
+                <input type="button" className="button black" value="." />
+                  <input type="button" className="button black" value="C" />
+                  <input type="button" className="button orange" value="=" />
+
+                  </p>
+              </div>
+            </div>
+          </div>
+    );
+  }
+}
+
+export default App;
