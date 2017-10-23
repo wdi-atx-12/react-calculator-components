1c8e8cb535730d2ca63645af9a083f27651f663f
Chris Johnson <jhnsn.c@gmail.com>
2017-10-23 15:35:16 -0500

    updating package versions, fixing indentation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
+  render() { // This is a mess! Let's break this down into components.
     return (
       <div className="App">
         <h1>React Calculator!</h1>
@@ -21,34 +21,32 @@ class App extends Component {
               <input type="button" className="button pink" value="/" />
             </p>
             <p>
-            <input type="button" className="button black" value="7" />
+              <input type="button" className="button black" value="7" />
               <input type="button" className="button black" value="8" />
-                <input type="button" className="button black" value="9" />
-                <input type="button" className="button pink" value="*" />
-              </p>
-              <p>
-                <input type="button" className="button black" value="4" />
-                <input type="button" className="button black" value="5" />
-                  <input type="button" className="button black" value="6" />
-                  <input type="button" className="button pink" value="-" />
-                  </p>
-                <p>
-                  <input type="button" className="button black" value="1" />
-                <input type="button" className="button black" value="2" />
-                  <input type="button" className="button black" value="3" />
-                  <input type="button" className="button pink" value="+" />
-
-                  </p>
-                <p>
-                  <input type="button" className="button black" value="0" />
-                <input type="button" className="button black" value="." />
-                  <input type="button" className="button black" value="C" />
-                  <input type="button" className="button orange" value="=" />
-
-                  </p>
-              </div>
-            </div>
+              <input type="button" className="button black" value="9" />
+              <input type="button" className="button pink" value="*" />
+            </p>
+            <p>
+              <input type="button" className="button black" value="4" />
+              <input type="button" className="button black" value="5" />
+              <input type="button" className="button black" value="6" />
+              <input type="button" className="button pink" value="-" />
+            </p>
+            <p>
+              <input type="button" className="button black" value="1" />
+              <input type="button" className="button black" value="2" />
+              <input type="button" className="button black" value="3" />
+              <input type="button" className="button pink" value="+" />
+            </p>
+            <p>
+              <input type="button" className="button black" value="0" />
+              <input type="button" className="button black" value="." />
+              <input type="button" className="button black" value="C" />
+              <input type="button" className="button orange" value="=" />
+            </p>
           </div>
+        </div>
+      </div>
     );
   }
 }
